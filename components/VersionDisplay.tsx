0d9a3ec749eef699e4b3b75a5ef0c8fe8bc6f63b
Mahendra Reddy <19jn1a0596@gmail.com>
2025-12-18 22:47:05 +0530

    last UI needed fixes

--- components/VersionDisplay.tsx
+++ components/VersionDisplay.tsx
@@ -1,12 +1,12 @@
+'use client'
 
-import packageInfo from '../package.json';
+import React from 'react'
+import packageJson from '../package.json'
 
-const VersionDisplay = () => {
+export default function VersionDisplay() {
   return (
-    <div className="fixed bottom-4 right-4 text-xs text-gray-500">
-      v{packageInfo.version}
+    <div className="fixed bottom-2 right-2 text-xs text-gray-400 bg-white/80 backdrop-blur px-2 py-1 rounded shadow-sm">
+      v{packageJson.version}
     </div>
-  );
-};
-
-export default VersionDisplay;
+  )
+}
